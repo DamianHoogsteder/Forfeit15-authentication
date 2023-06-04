f25f7582fe3bfb3613a63f9d36eb1cd68914c449
Damian <damian.hoogsteder@indicia.nl>
2023-06-04 11:09:34 +0200

    wip

--- Forfeit15.Auth.Core/Services/UserMetaData/UserMetaDataViewmodel.cs
+++ Forfeit15.Auth.Core/Services/UserMetaData/UserMetaDataViewmodel.cs
@@ -9,29 +9,29 @@ public class UserMetaDataViewmodel
     public Guid Id { get; set; }
         
     [DataMember] 
-    public string Name { get; set; }
-        
-    [DataMember(Name = "family_name")] 
-    public string FamilyName { get; set; }
-        
-    [DataMember(Name = "given_name")] 
-    public string GivenName { get; set; }
-        
-    [DataMember] 
-    public string NickName { get; set; }
-        
-    [DataMember] 
-    public string Email { get; set; }
-        
-    [DataMember] 
-    public string Locale { get; set; }
-        
-    [DataMember] 
-    public long Picture { get; set; }
-        
-    [DataMember] 
-    public string Sub { get; set; }
-        
-    [DataMember(Name = "updated_at")] 
-    public string UpdatedAt { get; set; }
+        public string Name { get; set; }
+            
+        [DataMember(Name = "family_name")] 
+        public string FamilyName { get; set; }
+            
+        [DataMember(Name = "given_name")] 
+        public string GivenName { get; set; }
+            
+        [DataMember] 
+        public string NickName { get; set; }
+            
+        [DataMember] 
+        public string Email { get; set; }
+            
+        [DataMember] 
+        public string Locale { get; set; }
+            
+        [DataMember] 
+        public long Picture { get; set; }
+            
+        [DataMember] 
+        public string Sub { get; set; }
+            
+        [DataMember(Name = "updated_at")] 
+        public string UpdatedAt { get; set; }
 }
